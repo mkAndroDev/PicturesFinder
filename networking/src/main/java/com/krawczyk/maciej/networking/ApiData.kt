43b0e77e6ae58418a793f5e109cc4f8e93c3a098
mkAndroDev <mkandrodev@gmail.com>
2023-02-11 19:16:26 +0100

    Cleanup in API Keys

--- networking/src/main/java/com/krawczyk/maciej/networking/ApiData.kt
+++ networking/src/main/java/com/krawczyk/maciej/networking/ApiData.kt
@@ -2,5 +2,5 @@ package com.krawczyk.maciej.networking
 
 internal object ApiData {
     const val BASE_URL = "https://pixabay.com/api/"
-    const val API_KEY = "33537726-76c116d79d970104c5eacd0e9"
+    const val API_KEY = "put your API key Here"
 }
